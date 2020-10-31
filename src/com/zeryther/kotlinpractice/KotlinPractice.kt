a62e14aa550a8e9d90e9218c71bc3856ff8d7e6e
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2020-11-01 00:20:15 +0100

    Initial commit

---
+++ src/com/zeryther/kotlinpractice/KotlinPractice.kt
@@ -0,0 +1,10 @@
+package com.zeryther.kotlinpractice
+
+class KotlinPractice {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello world!")
+        }
+    }
+}
