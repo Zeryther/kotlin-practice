65378e7f3d5fce5fcb22701cdabe76d764dba5e6
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2020-11-01 00:27:24 +0100

    Added list example

--- src/com/zeryther/kotlinpractice/KotlinPractice.kt
+++ src/com/zeryther/kotlinpractice/KotlinPractice.kt
@@ -5,6 +5,17 @@ class KotlinPractice {
         @JvmStatic
         fun main(args: Array<String>) {
             println("Hello world!")
+
+            val fruits = listOf("Banana", "Avocado", "Apple", "Kiwifruit")
+            fruits
+                .filter { this.startsWithIgnoreCase(it, "a") }
+                .sortedBy { it }
+                .map { it.toUpperCase() }
+                .forEach { println(it) }
+        }
+
+        fun startsWithIgnoreCase(text: String, check: String): Boolean {
+            return text.toLowerCase().startsWith(check.toLowerCase())
         }
     }
 }
